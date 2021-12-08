b6fc6a955415044d3d1eacef4f80b172e1cc82a9
sagar-joshi <sagar.00joshi@gmail.com>
2021-12-08 22:09:00 +0530

    add cors

--- server/server.js
+++ server/server.js
@@ -2,8 +2,9 @@ import dotenv from 'dotenv';
 import express from 'express';
 import session from 'express-session';
 import passport from 'passport';
-import './utils/auth.js';
+import cors from 'cors';
 
+import './utils/auth.js';
 import userRoutes from './routes/user.routes.js';
 import groupRoutes from './routes/group.routes.js';
 import messageRoutes from './routes/message.routes.js';
@@ -12,6 +13,7 @@ dotenv.config();
 const app = express();
 const port = process.env.PORT || 3000;
 
+app.use(cors());
 app.use(express.json());    //bodyparser middleware
 app.use(session({secret: process.env.SESSION_SECRET || "session_secret_here", resave: false, saveUninitialized: false}));
 app.use(passport.initialize());
