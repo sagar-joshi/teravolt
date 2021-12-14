302577ba205b79545cc93ea82c9e737054ecc368
sagar-joshi <sagar.00joshi@gmail.com>
2021-12-14 23:11:17 +0530

    fix scrollbar issue

--- client/src/components/MessageArea.js
+++ client/src/components/MessageArea.js
@@ -9,9 +9,9 @@ export function MessageArea(props){
         )
     })
     return (
-        // overflow-y-scroll is not defined by default in bootstrap 
-        // its defined in style.css and imported in App.js
-        <div className='h-100 overflow-auto d-flex flex-column-reverse'>
+        // added key attribute in the below div because outer div(s) were not re-rendering
+        // on msgList prop change, thus preventing the scrollbar to reach the bottom
+        <div key={props.msgList[0].name} className='h-100 overflow-auto d-flex flex-column-reverse'>
             <div className='MessageArea'>
                 {messages}
             </div>
