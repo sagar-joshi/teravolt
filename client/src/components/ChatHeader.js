02d2a790bf6840424f2bac6f3da017607ed32445
sagar-joshi <sagar.00joshi@gmail.com>
2021-12-15 21:16:00 +0530

    css change

--- client/src/components/ChatHeader.js
+++ client/src/components/ChatHeader.js
@@ -5,11 +5,11 @@ export function ChatHeader(props){
     }
     return (
         <div className="ChatHeader h-100 row">
-            <div className="col-1">
-                <button className="btn btn-close" onClick={handleClose}></button>
-            </div>
             <div className="col-11 d-flex justify-content-center">
                 {props.groupName}
+            </div>
+            <div className="col-1 d-flex justify-content-end">
+                <button className="btn btn-close" onClick={handleClose}></button>
             </div>
         </div>
     )
