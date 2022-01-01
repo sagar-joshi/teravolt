8685a84ee9427473c6522873167f748497cf7d8a
sagar-joshi <sagar.00joshi@gmail.com>
2022-01-01 21:18:34 +0530

    add zoom on hover on button icons

--- client/src/components/ChatHeader.js
+++ client/src/components/ChatHeader.js
@@ -48,18 +48,18 @@ export function ChatHeader(props){
             <div className="ChatHeader h-100 row d-flex flex-row align-items-center card">
                     <div className="col-10 d-flex justify-content-center align-items-center">
                         <IconContext.Provider value={{size: "1.5em", color: "blue"}}>
-                            <button className="btn" onClick={handlePrev}><MdNavigateBefore/></button>
+                            <button className="btn" onClick={handlePrev}><MdNavigateBefore className='zoom-on-hover'/></button>
                         </IconContext.Provider>
                         <IconContext.Provider value={{size: "1.5rem", color: "green"}}>
                             {props.groupName} <span className='ms-3'><MdPeople/></span>{activeCount}
                         </IconContext.Provider>
                         <IconContext.Provider value={{size: "1.5em", color: "blue"}}>
-                            <button className="btn" onClick={handleNext}><MdNavigateNext/></button>
+                            <button className="btn" onClick={handleNext}><MdNavigateNext className='zoom-on-hover'/></button>
                         </IconContext.Provider>
                     </div>
                 <div className="col-2 d-flex justify-content-end">
                     <IconContext.Provider value={{size: "1.5rem", color: "red"}}>
-                        <button className="btn" onClick={handleClose}><MdClose/></button>
+                        <button className="btn" onClick={handleClose}><MdClose className="zoom-on-hover"/></button>
                     </IconContext.Provider>
                 </div>
             </div>
